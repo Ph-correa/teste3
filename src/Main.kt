d6b2902f81f09507732f0bc339e9fe8a402b4991
Ph-correa <pedrohcorrea@uniplaclages.edu.br>
2025-11-12 14:09:12 -0300

    nome compleyo

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,9 @@
+
 fun main(){
     val matriz = Int
     println("Digite seu email")
     var email = readln()
+    var nome_copleto = StringBuilder()
+    nome_copleto.append("pedrao")
+    nome_copleto.append("henrique")
 }
